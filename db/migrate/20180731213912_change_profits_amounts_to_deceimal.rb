5c17021d43b39e75a82b5e230690a21c07d67923
Malachi Irwin <malachi@theirwins.ws>
2018-08-01 16:35:35 -0500

    Delete invalid migration

--- db/migrate/20180731213912_change_profits_amounts_to_deceimal.rb
+++
@@ -1,6 +0,0 @@
-class ChangeProfitsAmountsToDeceimal < ActiveRecord::Migration[5.1]
-  def change
-    change_column :profits, :dozens_bought, :decimal{10,2}
-    change_column :profits, :amount, :decimal{10,2}
-  end
-end
